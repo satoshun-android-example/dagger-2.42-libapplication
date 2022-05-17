b4760d08ebcc40fe68873553593889511a6d1fc9
SatoShun <shun.sato1@gmail.com>
2022-05-17 06:32:51 +0000

    dagger

--- app/src/main/java/io/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/io/github/satoshun/example/AppActivity.kt
@@ -1,7 +1,24 @@
 package io.github.satoshun.example
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class AppActivity : AppCompatActivity()
+class AppActivity : AppCompatActivity() {
+  @Inject lateinit var test: Test
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    test.show()
+  }
+}
+
+class Test @Inject constructor() {
+  fun show() {
+    println("test")
+  }
+}
